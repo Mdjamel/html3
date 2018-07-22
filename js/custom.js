13c8c13671c1e18d626051a18a9c042d95504fa7
mdjamel <mdjaich@gmail.com>
2018-07-22 16:36:05 +0200

    Init

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+/*global $,alert, console */
+$(function () {
+    'use strict';
+    //Trigger niceScroll
+    $('html').niceScroll();
+    //Change header height
+
+    $('.header').height($(window).height());
+
+    //Scroll To Features
+    $('.header .arrow i').click(function(){
+        $('html, body').animate({
+            scrollTop: $('.features').offset().top   
+        },500);
+        
+    })
+  
+
+
+
+
+})
